3d4fdbe54265b84e2dbad8afd2125e61a31d487b
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-12-01 22:52:34 +0100

    Update Product.java

--- app/src/main/java/com/example/tp_4/models/Product.java
+++ app/src/main/java/com/example/tp_4/models/Product.java
@@ -15,4 +15,32 @@ public class Product extends RealmObject {
     public long getId() {
         return id;
     }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getP_intitule() {
+        return p_intitule;
+    }
+
+    public void setP_intitule(String p_intitule) {
+        this.p_intitule = p_intitule;
+    }
+
+    public float getP_prix() {
+        return p_prix;
+    }
+
+    public void setP_prix(float p_prix) {
+        this.p_prix = p_prix;
+    }
+
+    public int getId_image() {
+        return id_image;
+    }
+
+    public void setId_image(int id_image) {
+        this.id_image = id_image;
+    }
 }
