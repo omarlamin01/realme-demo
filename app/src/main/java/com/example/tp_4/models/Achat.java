4c5df7ce2e759b9c6d39c1072c9713c4223e3478
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-12-01 22:55:12 +0100

    Update Achat.java

--- app/src/main/java/com/example/tp_4/models/Achat.java
+++ app/src/main/java/com/example/tp_4/models/Achat.java
@@ -4,10 +4,21 @@ import java.util.Date;
 
 import io.realm.RealmList;
 import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
 
 public class Achat extends RealmObject {
+    @PrimaryKey
+    private long id;
     private RealmList<Product> products;
     private Date date;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public RealmList<Product> getProducts() {
         return products;
