1e4261afb345405665fadda938586cedaa991adc
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-12-01 23:17:22 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/tp_4/MainActivity.java
+++ app/src/main/java/com/example/tp_4/MainActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.tp_4.models.Achat;
+import com.example.tp_4.models.Product;
+
 import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class MainActivity extends AppCompatActivity {
     private Realm realm;
@@ -14,6 +18,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         realm = Realm.getDefaultInstance();
+        realm.beginTransaction();
+        Product produit = realm.createObject(Product.class, 1L);
+        produit.setP_intitule("Ordinateur HP");
+        produit.setP_prix(5);
+        Achat achat = realm.createObject(Achat.class);
+        achat.addProduct(produit);
+        realm.commitTransaction();
+        RealmResults<Product> results = realm
+                .where(Product.class)
+                .findAll();
+        for(Product item :results) {
+            System.out.println("****************************" + item.toString());
+        }
     }
 
     @Override
